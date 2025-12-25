c7aaf1cf51fa2a86ead394f1cffa244fae5b4aee
Pratham Nayak <s4062599@student.rmit.edu.au>
2025-12-25 10:53:31 +0530

    Added memory model, owned by CPU wrapper

--- src/main.cpp
+++ src/main.cpp
@@ -22,14 +22,35 @@ void emulateCycle()
 struct CPUState
 {
     using RegisterType = uint16_t;
+    using RAMType = uint16_t;
     /*
     Here we treat the registers as a trusted architectural state.
     We use uint16_t (2 byte registers) to avoid problems that may arise due to overflow.
-    The CPU is always created in a known, deterministic manner, so we can avoid using constructors here.
+    The CPU state is always created in a known, deterministic manner, 
+    so we can avoid using constructors here.
     */
-    std::array<RegisterType, 4> GenPR; // General Purpose Registers.
+    std::array<RegisterType, 4> GenPR{}; // General Purpose Registers.
 
-    uint8_t pc; // Program Counter
+    //We use size_t here (host agnostic) keeping in mind that PC Width >= memory index width
+    size_t pc; // Program Counter
+    
     uint8_t sf; // Status flag
+
+    /*
+    We model memory as a dumb, plain architectural state.
+    Instructions are word-encoded (16 bit)
+    The memory isn't a separate device, it is owned by the CPU wrapper,
+    and accessed via free functions.
+    */
+
+    static constexpr size_t RAM_SIZE = 64 * 1024; // 128KB RAM for 16-bit architecture.
+    std::array<RAMType, RAM_SIZE> data{};
     
 };
+
+/*
+This project is a sequential interpreter and does not require an explicit state machine
+to model the atomic cycle of computation, hence it is omitted.
+The control flow encodes the state.
+*/
+
